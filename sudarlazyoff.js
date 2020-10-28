2e00c854f5f4a55631fff5f999212eda1f454ba3
SudarBlogger <71109343+SudarBlogger@users.noreply.github.com>
2020-10-28 07:39:40 +0700

    Update sudarlazyoff.js

--- sudarlazyoff.js
+++ sudarlazyoff.js
@@ -9,4 +9,4 @@ setTimeout(function(){$(".video-youtube").each(function(){$(this).replaceWith('<
 // Disqus One
 !function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="//"+disqus_shortname+".disqus.com/blogger_item.js",(document.getElementsByTagName("head")[0]||document.getElementsByTagName("body")[0]).appendChild(e)}();
 // Disqus
-function load_Comments(){var e=document.getElementById("disqusshow");e.style.display="none";var t="malas-menulis";!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://"+t+".disqus.com/embed.js",(document.getElementsByTagName("head")[0]||document.getElementsByTagName("body")[0]).appendChild(e)}()};
+function load_Comments(){var e=document.getElementById("disqusshow");e.style.display="none";var t="malasmenulis";!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://"+t+".disqus.com/embed.js",(document.getElementsByTagName("head")[0]||document.getElementsByTagName("body")[0]).appendChild(e)}()};
